db39f4da48dcb7dab446bc1c230cada2f1504c5d
Kong Somvannda <530650+somvannda@users.noreply.github.com>
2026-09-09 09:44:05 +0700

    feat(web): adopt react-router (BrowserRouter + useNavigate) in place of manual history calls

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./components/ui";
 import "./styles.css";
@@ -30,7 +31,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppErrorBoundary>
       <ThemeProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </AppErrorBoundary>
   </React.StrictMode>,
